06806be4d9479054b5c3948d874b66ad0c60a2fd
jasmin-alejandra <jasmin-ag88@hotmail.com>
2025-11-29 23:16:13 -0500

    Added AI API and functions to generate dicho

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function displayDicho(response) {
+  new Typewriter(".dicho", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 3,
+    cursor: "",
+  });
+}
 
+function generateDicho(event) {
+  event.preventDefault();
+
+  let apiKey = "a02f35oaf431a4c16ab5t443397e311f";
+  let prompt =
+    "Provide a dicho, a saying in Spanish, about the subject provided in the context. Explain the dicho in english.";
+  let subjectInput = document.querySelector("#subject");
+  let context = subjectInput.value;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  console.log(context);
+  console.log("generating poem");
+  axios.get(apiUrl).then(displayDicho);
+}
+let dichoFormElement = document.querySelector("#dicho-form");
+dichoFormElement.addEventListener("submit", generateDicho);
